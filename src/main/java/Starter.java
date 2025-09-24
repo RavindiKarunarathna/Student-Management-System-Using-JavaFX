7da48665c25619d91bd156f6787363f8d4d5dfee
RavindiKarunarathna <ravindiicet@gmail.com>
2025-09-24 14:31:52 +0530

    Inititial commit

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -3,7 +3,7 @@ import javafx.stage.Stage;
 
 public class Starter extends Application {
     public static void main(String[] args){
-
+        
     }
     @Override
     public void start(Stage stage) throws Exception {
